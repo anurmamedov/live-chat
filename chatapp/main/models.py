11bdb2b08ea0c3e9ffda47db7f4160f6dff19e29
anurmamedov <sdeveloperanar@gmail.com>
2024-08-25 23:10:40 -0400

    first migration

--- chatapp/main/models.py
+++ chatapp/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.TextField(max_length=20)
