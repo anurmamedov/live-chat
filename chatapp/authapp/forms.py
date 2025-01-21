3d4f726ffcba1a0a227b9e5cb15b0a57ea322add
anurmamedov <sdeveloperanar@gmail.com>
2025-01-20 23:05:01 -0500

    avatar added in models

--- chatapp/authapp/forms.py
+++ chatapp/authapp/forms.py
@@ -1,8 +1,32 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from main.models import Profile
+
 
 class SignUpForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    avatar = forms.ImageField(required=False)  # Avatar field
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.set_password(self.cleaned_data["password"])
+            user.save()
+            # Create a Profile for the user (including avatar if provided)
+            profile = Profile.objects.create(user=user)
+            if 'avatar' in self.cleaned_data:
+                profile.avatar = self.cleaned_data['avatar']
+                profile.save()
+        return user
--- chatapp/main/models.py
+++ chatapp/main/models.py
@@ -14,3 +14,11 @@ class Message(models.Model):
 
     def __str__(self):
        return self.sender.username + " : " + self.content
+    
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
