20f966c57392fefdcab4b46abe8285cd48a96ef8
anurmamedov <sdeveloperanar@gmail.com>
2025-01-20 22:09:20 -0500

    displaying messages in console

--- chatapp/main/views.py
+++ chatapp/main/views.py
@@ -19,10 +19,11 @@ def main_view(request):
 
 def display_messages(request):
     print(1)
-    all_messages = Message.objects.all()
-    json_data = serialize('json', all_messages)
-    print(json_data)
-    return JsonResponse({'data': json_data}, status=200)
+    # all_messages = Message.objects.values('sender', 'content', 'date')
+    all_messages = Message.objects.select_related('sender').values('sender__username', 'content', 'date')
+    all_messages_list = list(all_messages)
+    print(all_messages_list)
+    return JsonResponse({'data': all_messages_list}, status=200)
 
 
 @csrf_exempt
