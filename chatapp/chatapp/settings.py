653cd40f48b3c342ba4a926d0a700083bb81b493
anurmamedov <sdeveloperanar@gmail.com>
2025-03-11 18:40:07 -0400

    avatar picture updated

--- chatapp/chatapp/settings.py
+++ chatapp/chatapp/settings.py
@@ -46,13 +46,13 @@ INSTALLED_APPS = [
 ]
 
 
-
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http:// localhost:8000',  # Replace with your frontend domain
 ]
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
